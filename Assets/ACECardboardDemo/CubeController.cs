f3cbd2084573a474f4373b5de1b23c4ac1238f9a
ace <andrew.emil@gmail.com>
2017-12-01 14:07:02 -0500

    Initial and final commit lol

---
+++ Assets/ACECardboardDemo/CubeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	private Material material;
+	void Start () {
+		material = GetComponent<Renderer> ().material;
+	}
+
+	public void PointerEnterHandler() {
+		material.SetColor ("_Color", Color.green);
+	}
+
+	public void PointerExitHandler() {
+		material.SetColor ("_Color", Color.blue);
+	}
+
+	public void PointerClickHandler() {
+		material.SetColor ("_Color", Color.red);
+	}
+}
